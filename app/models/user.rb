a2930d7a683a70f643be6f5ece2f458f6a50ebb7
baodoanusth <baodoan1131996@gmail.com>
2016-07-04 09:56:17 +0700

    Finish toy app

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ActiveRecord::Base
+	has_many :microposts
+	validates :content, presence: true
+    validates :user_id, presence: true
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :microposts
+  resources :microposts
+  resources :users
   root 'application#hello'
 end
